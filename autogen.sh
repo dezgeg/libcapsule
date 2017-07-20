5670e9bd9603c44208fa83739002bed6cd3aa935
Vivek Das Mohapatra <vivek@collabora.co.uk>
2017-07-20 15:35:39 +0100

    Hook up an autogen.sh

---
+++ autogen.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+# Run this to generate all the initial makefiles, etc.
+
+[ -n "$srcdir" ] || srcdir=$(dirname "$0");
+[ -n "$srcdir" ] || srcdir=.;
+
+( cd $srcdir
+
+  if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ];
+  then
+      (echo "*** WARNING: Running 'configure' with no arguments.";
+       echo "*** configure arguments should be passed as:"       ;
+       echo "*** $0 ARGS..."                                     ;
+       echo ""                                                   ) >&2;
+  fi;
+  autoreconf -ivf || exit $?; );
+
+if [ "$NOCONFIGURE" = "" ];
+then
+    $srcdir/configure "$@" || exit $?;
+
+    if [ "$1" = "--help" ]; then exit 0; fi;
+    echo "Now type 'make' to compile";
+else
+    echo "Skipping configure process.";
+fi
