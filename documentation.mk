3c4006e9059af086917c58a94f2f69cedd116bab
Vivek Das Mohapatra <vivek@collabora.co.uk>
2017-07-21 15:57:00 +0100

    Move gtk-doc config into Makefile.am
    It must live in a configure-generated file,
    not in the static documentation.mk

--- documentation.mk
+++ documentation.mk
@@ -15,28 +15,6 @@ XSLT_MAN = http://docbook.sourceforge.net/release/xsl/current/manpages/docbook.x
 
 %.3: xml/%.xml docs
 	$(AM_V_GEN) $(XSLTPROC) $(XSLTPROC_FLAGS) $(XSLT_MAN) $<
-# ============================================================================
-# gtk-doc configuration: see /usr/share/doc/gtk-doc-tools/examples/Makefile.am
-DOC_MODULE           = libcapsule
-DOC_MAIN_SGML_FILE   = $(DOC_MODULE)-docs.xml
-DOC_SOURCE_DIR       = capsule
-SCAN_OPTIONS         =
-MKDB_OPTIONS         = --xml-mode --output-format=xml
-MKTMPL_OPTIONS       =
-MKHTML_OPTIONS       =
-FIXXREF_OPTIONS      =
-HFILE_GLOB           =
-CFILE_GLOB           =
-EXTRA_HFILES         =
-IGNORE_HFILES        =
-HTML_IMAGES          =
-content_files        =
-expand_content_files =
-GTKDOC_CFLAGS        =
-GTKDOC_LIBS          =
 
--include gtk-doc.make
-
-# ============================================================================
 man_MANS    = capsule-init-project.1 capsule-mkstublib.1
 CLEANFILES += $(man_MANS)
